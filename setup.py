46396d41060f19e7f48d1c8eca9faf656b8f7eb5
Adam Coddington <adam.coddington@coxinc.com>
2015-01-28 15:50:12 -0800

    Release 0.3; adding debugging functionality.

--- setup.py
+++ setup.py
@@ -1,8 +1,31 @@
+import os
 from setuptools import setup, find_packages
+import uuid
+
+requirements_path = os.path.join(
+    os.path.dirname(__file__),
+    'requirements.txt',
+)
+try:
+    from pip.req import parse_requirements
+    requirements = [
+        str(req.req) for req in parse_requirements(
+            requirements_path,
+            session=uuid.uuid1()
+        )
+    ]
+except ImportError:
+    requirements = []
+    with open(requirements_path, 'r') as in_:
+        requirements = [
+            req for req in in_.readlines()
+            if not req.startswith('-')
+            and not req.startswith('#')
+        ]
 
 setup(
     name='taskwarrior-timebook-hook',
-    version='0.2.1',
+    version='0.3',
     url='https://github.com/coddingtonbear/taskwarrior-timebook-hook',
     description=(
         'Track your tasks as Timebook entries',
@@ -13,10 +36,12 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
+    install_requires=requirements,
     packages=find_packages(),
     entry_points={
         'console_scripts': [
-            'taskwarrior_timebook_hook = taskwarrior_timebook_hook:cmdline'
+            'taskwarrior_timebook_hook = taskwarrior_timebook_hook:cmdline',
+            'taskwarrior_timebook_hook_debug = taskwarrior_timebook_hook:cmdline_debug'
         ],
     },
 )
--- taskwarrior_timebook_hook/__init__.py
+++ taskwarrior_timebook_hook/__init__.py
@@ -1,15 +1,32 @@
 import json
+import logging
+import os
 import sys
-import subprocess
+import subprocess32 as subprocess
+import tempfile
+
+
+logger = logging.getLogger(__name__)
+
+
+def execute_command(args, call_type='call', **kwargs):
+    method = getattr(subprocess, call_type)
+    logger.debug("Executing subprocess: %s (%s).", args, call_type)
+    value = method(args, **kwargs)
+    if isinstance(value, basestring):
+        value = value.strip()
+    logger.debug("Subprocess returned with value: %s", value)
+    return value
 
 
 def set_current_task(description=''):
-    returncode = subprocess.call([
+    returncode = execute_command([
         't',
         'change',
         description
     ])
     if returncode != 0:
+        logger.warning("Not currently clocked-in; no timebook entry made.")
         print "You are not currently clocked-in"
 
 
@@ -17,19 +34,55 @@ def main(stdin):
     lines = stdin.split('\n')
     original = json.loads(lines[0])
     modified = json.loads(lines[1])
+    logger.debug(
+        "Timebook version: %s",
+        execute_command(['t', '--version'], call_type='check_output')
+    )
+    logger.debug(
+        "Taskwarrior version: %s",
+        execute_command(['task', '--version'], call_type='check_output')
+    )
+    logger.debug("Original task: %s", original)
+    logger.debug("Modified task: %s", modified)
 
     if 'start' in original and 'start' not in modified:
         # Stop tracking time to this task.
+        logger.info(
+            "Task %s no longer active; stopping current task.",
+            original['uuid'],
+        )
         set_current_task()
     elif 'start' in modified and 'start' not in original:
         # Start tracking time to this task.
+        logger.info(
+            "Task %s started; setting timebook entry as \"%s\".",
+            original['uuid'],
+            modified['description'],
+        )
         set_current_task(modified['description'])
 
-    return json.dumps(modified, separators=(',', ':'))
+    output = json.dumps(modified, separators=(',', ':'))
+    logger.debug(
+        "Returning output %s to taskwarrior.",
+        output,
+    )
+    return output
 
 
-def cmdline():
+def cmdline(debug=False):
+    if debug:
+        logging.basicConfig(
+            level=logging.DEBUG,
+            filename=os.path.join(
+                tempfile.gettempdir(),
+                'taskwarrior_timebook_hook.log',
+            )
+        )
     sys.stdout.write(main(sys.stdin.read()))
+
+
+def cmdline_debug():
+    cmdline(debug=True)
 
 
 if __name__ == '__main__':
